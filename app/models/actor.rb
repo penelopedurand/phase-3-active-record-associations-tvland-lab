f29ac9c03df0d8acb028612f34968c927a2f6020
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-05-23 22:10:34 -0400

    commit tree fa4f3f6f6b9b83caedb66f276dccd157b7ad1580

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+        #that lists all of the characters that actor has alongside the show that the character is in.
+        characters.map do |character|
+            "#{character.name} - #{character.show.name}" 
+        end
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters 
+  has_many :actors, through: :characters
+  belongs_to :network 
+
+  def actors_list
+    #that returns an Array of the full names of each actor associated with a show. 
+    actors.map do |actor|
+        actor.full_name 
+    end
+  end 
 end
